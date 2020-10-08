832d8e659c89557b3f39b5b366a0262d4772c27c
SzklevanMilan <szklevanm@gmail.com>
2020-10-08 09:24:18 +0200

    CímEggyelNo metódus elkészítése

--- dhcp_szimulacio/Program.cs
+++ dhcp_szimulacio/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,65 @@ namespace dhcp_szimulacio
 {
     class Program
     {
+        static List<string> excluded = new List<string>();
+
+        static void BeolvasExcluded()
+        {
+            try
+            {
+                StreamReader file = new StreamReader("excluded.csv");
+                try
+                {
+                    while (!file.EndOfStream)
+                    {
+                        excluded.Add(file.ReadLine());
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                finally 
+                {
+                    file.Close();
+                }
+
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static string CimEggyelNo(string cim)
+        {
+            /* cim = 192.168.10.100
+             * return = 192.168.10.101
+             * szétvágni '.'
+             * az utolsót int-é konvertálni és egyet hozzáadni
+             * (255 ne lépjük túl)
+             * összefűzni string-é
+             */
+            string[] adat = cim.Split('.');
+            int okt4 = int.Parse(adat[3]);
+            if (okt4<255)
+            {
+                okt4++;
+            }
+            return adat[0]+"."+adat[1]+"."+adat[2]+"."+okt4.ToString();
+
+        }
         static void Main(string[] args)
         {
+            BeolvasExcluded();
+            //foreach (var e in excluded)
+            //{
+            //    Console.WriteLine(e);
+            //}
 
+            Console.WriteLine("\nVége...");
+            Console.ReadLine();
         }
     }
 }
